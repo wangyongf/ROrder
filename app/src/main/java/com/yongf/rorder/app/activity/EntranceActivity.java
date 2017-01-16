3277a779a9821a749a8ea22c8dcfd21c891b9f6e
Scott Wang <scottwang1996@gmail.com>
2017-01-16 20:30:25 +0800

    增加EntranceActivity，方便后续开发时调试各个页面

---
+++ app/src/main/java/com/yongf/rorder/app/activity/EntranceActivity.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 1996-2016 YONGF Inc.All Rights Reserved.
+ * Scott Wang blog.54yongf.com | blog.csdn.net/yongf2014	
+ * 文件名: EntranceActivity						
+ * 描述: 								
+ * 修改历史: 
+ * 版本号    作者                日期              简要介绍相关操作
+ *  0.1         Scott Wang     17-1-16       新增：Create	
+ */
+
+package com.yongf.rorder.app.activity;
+
+import com.yongf.rorder.R;
+import com.yongf.rorder.util.IntentHelper;
+
+import butterknife.OnClick;
+
+/**
+ * EntranceActivity
+ *
+ * @author Scott Wang
+ * @version 1.0, 17-1-16
+ * @see
+ * @since ROder V0.1
+ */
+public class EntranceActivity extends BaseActivity {
+
+    private static final String TAG = "EntranceActivity";
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_entrance;
+    }
+
+    @OnClick(R.id.entrance_activity)
+    public void go2Entrance() {
+        IntentHelper.simpleJump(this, EntranceActivity.class);
+    }
+
+    @OnClick(R.id.feedback_activity)
+    public void go2Feedback() {
+        IntentHelper.simpleJump(this, FeedbackActivity.class);
+    }
+
+    @OnClick(R.id.find_pwd_activity)
+    public void go2FindPwd() {
+        IntentHelper.simpleJump(this, FindPwdActivity.class);
+    }
+
+    @OnClick(R.id.login_activity)
+    public void go2Login() {
+        IntentHelper.simpleJump(this, LoginActivity.class);
+    }
+
+    @OnClick(R.id.main_activity)
+    public void go2Main() {
+        IntentHelper.simpleJump(this, MainActivity.class);
+    }
+
+    @OnClick(R.id.register_activity)
+    public void go2Register() {
+        IntentHelper.simpleJump(this, RegisterActivity.class);
+    }
+
+    @OnClick(R.id.splash_activity)
+    public void go2Splash() {
+        IntentHelper.simpleJump(this, SplashActivity.class);
+    }
+}
---
+++ app/src/main/java/com/yongf/rorder/util/IntentHelper.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 1996-2016 YONGF Inc.All Rights Reserved.
+ * Scott Wang blog.54yongf.com | blog.csdn.net/yongf2014	
+ * 文件名: IntentHelper						
+ * 描述: 								
+ * 修改历史: 
+ * 版本号    作者                日期              简要介绍相关操作
+ *  0.1         Scott Wang     17-1-16       新增：Create	
+ */
+
+package com.yongf.rorder.util;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * IntentHelper
+ *
+ * @author Scott Wang
+ * @version 1.0, 17-1-16
+ * @see
+ * @since ROder V0.1
+ */
+public class IntentHelper {
+
+    /**
+     * 简单地从当前页面跳转到指定页面
+     *
+     * @param context
+     * @param clazz
+     */
+    public static void simpleJump(Context context, Class clazz) {
+        Intent intent = new Intent(context, clazz);
+        context.startActivity(intent);
+    }
+}
