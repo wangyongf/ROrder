1e21fb97fe26d5df4f4b6d51c25fd2ee6a83ccd3
dingguo <dingguo@wacai.com>
2017-02-24 18:19:15 +0800

    引入MaterialSearchView

--- app/src/main/java/com/yongf/rorder/app/activity/SearchActivity.java
+++ app/src/main/java/com/yongf/rorder/app/activity/SearchActivity.java
@@ -1,6 +1,19 @@
 package com.yongf.rorder.app.activity;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 import com.yongf.rorder.R;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 /**
  * @author dingguo@wacai.com
@@ -9,10 +22,95 @@ import com.yongf.rorder.R;
 
 public class SearchActivity extends BaseActivity {
 
+    // FIXME: 2017/2/24 跑一下官方Demo，看看是哪里的问题！
+
     private static final String TAG = SearchActivity.class.getSimpleName();
+
+    @BindView(R.id.fl_toolbar)
+    FrameLayout mFlToolbar;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.search_view)
+    MaterialSearchView mSearchView;
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_search;
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        mSearchView.setVoiceSearch(false);
+        mSearchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
+    }
+
+    @Override
+    protected void initEvent() {
+        super.initEvent();
+
+        mSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        mSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        mSearchView.setMenuItem(item);
+
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    mSearchView.setQuery(searchWrd, false);
+                }
+            }
+
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mSearchView.isSearchOpen()) {
+            mSearchView.closeSearch();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
