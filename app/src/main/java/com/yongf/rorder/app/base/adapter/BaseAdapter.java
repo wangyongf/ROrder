773c287c0841cb1ee1bd667ec5c67a819a151419
dingguo <dingguo@wacai.com>
2017-03-08 11:05:49 +0800

    修改应用名称为美味宝

---
+++ app/src/main/java/com/yongf/rorder/app/base/adapter/BaseAdapter.java
@@ -0,0 +1,9 @@
+package com.yongf.rorder.app.base.adapter;
+
+/**
+ * @author dingguo@wacai.com
+ * @since 2017/3/8
+ */
+
+public class BaseAdapter {
+}
---
+++ app/src/main/java/com/yongf/rorder/component/dialog/BaseDialog.java
@@ -0,0 +1,9 @@
+package com.yongf.rorder.component.dialog;
+
+/**
+ * @author dingguo@wacai.com
+ * @since 2017/3/8
+ */
+
+public class BaseDialog {
+}
