479573f121ac31b5d3d850dc19b4b6c60aeb55ad
dingguo <dingguo@wacai.com>
2017-03-09 14:59:57 +0800

    广场页面布局初步完成

--- app/src/main/java/com/yongf/rorder/app/fragment/HotFragment.java
+++ app/src/main/java/com/yongf/rorder/app/fragment/HotFragment.java
@@ -43,7 +43,7 @@ public class HotFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_hot, container, false);
+        View view = inflater.inflate(R.layout.fragment_square, container, false);
         return view;
     }
 }
